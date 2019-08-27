d6ae2100b97d18157a6a58eae0885fb7808dc7eb
biomedicalGenomicsCNAG <52568402+biomedicalGenomicsCNAG@users.noreply.github.com>
2019-08-27 17:23:02 +0200

    Update SequenceLogos.R

--- SequenceContext/Code/SequenceLogos.R
+++ SequenceContext/Code/SequenceLogos.R
@@ -1,4 +1,3 @@
-
 #################################
 ###    SEQUENCE LOGO PLOTS    ###
 #################################
@@ -10,29 +9,68 @@ library(Biostrings)
 library(ggplot2)
 library(ggseqlogo)
 
-# Prior genome 
-fastafileGenome # need to load genome in fasta format
-genome_dnastringset <- readDNAStringSet(fastafileGenome)
-genome_dnastringset <- genome_dnastringset[names(genome_dnastringset) %in% c(1:22, "X", "Y")]
-all_motifs_counts <- oligonucleotideFrequency(genome_dnastringset, width=1, step=1)
-num_base <- colSums(all_motifs_counts)
-prior_genome <- num_base/sum(num_base)  #("A": 0.2952696, "C": 0.2044550, "G": 0.2045758, "T": 0.2956996)
 
-# FUNCTION: "plotLogo21"
+#' getPriorGenome
+#'
+#' @description Get the prior of the four nucleotides in the genome
+#' @param fastafileGenome genome sequence in fasta format
+#'
+#' @return prior_genome
+#'
+#' @examples
+getPriorGenome <- function(fastafileGenome){
+  
+  genome_dnastringset <- readDNAStringSet(fastafileGenome)
+  genome_dnastringset <- genome_dnastringset[names(genome_dnastringset) %in% c(1:22, "X", "Y")]
+  all_motifs_counts <- oligonucleotideFrequency(genome_dnastringset, width=1, step=1)
+  num_base <- colSums(all_motifs_counts)
+  prior_genome <- num_base/sum(num_base)
+  
+  return(prior_genome)
+  
+}
 
-# Description: Plots sequence logo by using ggseqlogo (which in turn makes use of ggolot2)
- 
-# Arguments (this function is call in the next function and all the next parameters will be defined along the function)
-##   data:character vector of sequences of the same length
-##   sequence_length: length of the sequence to be plotted
-##   yminrectsubtype: height of the bottom line for the box where the mutation subtype will be indicated.
-##   ymaxrectsubtype: height of the top line for the box where the mutation subtype will be indicated.
-##   ytext: height coordinate to plot the mutation subtype.
-##   group: specific cluster wanted to be analysed (e.g. in case of Cluster A, provide: "cluster_A").
-##   subtype: mutation type (e.g. for evaluating C>A mutations, provide: "C_A" ).
-##   n_commas: number of sequences used to build the sequence logo (Formatted using comma for the thousand position)
+
+#' countingSeqContext
+#' 
+#' @description Get nucleotide count per base at each position of the sequence
+#' @param group specific cluster to be analysed (e.g. in case of Cluster A, provide: "cluster_A").
+#' @param subtype mutation type: c("C_A", "C_G", "C_T", "T_A", "T_C", "T_G") (e.g. for evaluating C>A mutations, provide: "C_A" ).
+#' @param selection_case "rec" if wanted to analyse recurrent mutations or "unique" if wanted to analyse non-recurrent mutations.
+#' @param num_bp_context how many bases to extend next to the mutation (going in one direction, the same length will be used for the opposite direction). 
+#' @param sequences character vector of sequences of the same length
+#'
+#' @return seq_context_counts
+#'
+countingSeqContext <- function(group, subtype, selection_case, num_bp_context, sequences){
+  
+  # Subset the sequence
+  centreMut <- ceiling(nchar(sequences$seq_context_250_bp_pyr[1])/2) #ssm_pyr_standard (variable to choose which case to study, tener en cuenta que no uso el standard tengo que cambiar como calculo Rfreq porque tengo dos nucleotidos en el centro)
+  seq_context_data <- substr(sequences$seq_context_250_bp_pyr, centreMut-num_bp_context, centreMut+num_bp_context)
+  
+  # Do the counts
+  seq_context_counts <- consensusMatrix(seq_context_data, as.prob=FALSE)
+  
+  return(seq_context_counts)
+  
+}
 
 
+
+#' plotLogo21
+#'
+#' @description Plots sequence logo by using ggseqlogo (which in turn makes use of ggolot2)
+#' @param data relative frequencies per base and position
+#' @param sequence_length length of the sequence to 
+#' @param yminrectsubtype height of the bottom line for the box where the mutation subtype will be indicated.
+#' @param ymaxrectsubtype height of the top line for the box where the mutation subtype will be indicated.
+#' @param ytext height coordinate to plot the mutation subtype.
+#' @param group specific cluster wanted to be analysed (e.g. in case of Cluster A, provide: "cluster_A").
+#' @param subtype mutation type: c("C_A", "C_G", "C_T", "T_A", "T_C", "T_G") (e.g. for evaluating C>A mutations, provide: "C_A" ).
+#' @param n_commas number of sequences used to build the sequence logo (Formatted using comma for the thousand position)
+#'
+#' @return basic_LogoPlot
+#'
 plotLogo21 <- function(data, sequence_length, yminrectsubtype, ymaxrectsubtype, ytext, group, subtype, n_commas){
   
   basic_LogoPlot <- ggseqlogo(data, method="custom", seq_type='dna') +
@@ -64,42 +102,39 @@ plotLogo21 <- function(data, sequence_length, yminrectsubtype, ymaxrectsubtype, 
 
 
 
-# FUNCTION: "SequenceLogo21"
-
-# Description: Computes relative entropy per base in a position of the sequence and total entropy in that position from a matrix of counts of nucleotide per position,
-#             builds the correspondig sequence Logo and highlights any position enriched.
-
-# Arguments
-##    file: file name of a matrix with the counts of each nucleotide pero position of the aligned sequences to stud. Needed a file in .RData format.
-##    length_aroundMut: how many bases to extend next to the mutation (going in one direction, the same length will be used for the opposite direction).
-##    threshold: minimum information content (measured in total entropy) to consider enrichment in a position of the sequence.
-##    group: specific cluster wanted to be analysed (e.g. in case of Cluster A, provide: "cluster_A").
-##    subtype: mutation type (e.g. for evaluating C>A mutations, provide: "C_A" ).
-##    selection_case: "rec" if wanted to analyse recurrent mutations or "unique" if wanted to analyse non-recurrent mutations.
-##    prior genome: corresponding proportion of each nucleotide in the genome.
-##    Dir_toSave_logoplot: directory to save the sequence logo plot.
-
-
-
-SequenceLogo21 <- function(file, length_aroundMut, threshold, group, subtype, selection_case, prior_genome, Dir_toSave_logoplot){
-
-  # 1. LOAD DATA and assign some variables
-    # data counts
+#' SequenceLogo21
+#'
+#' @description Computes relative entropy per base in a position of the sequence and total entropy in that position from a matrix of counts of nucleotide per position, builds the correspondig sequence Logo and highlights any position enriched.
+#' @param file matrix (R object) with the counts of each nucleotide per position of the aligned sequences
+#' @param num_bp_context how many bases to extend next to the mutation (going in one direction, the same length will be used for the opposite direction). 
+#' @param threshold value of information content (measured in total entropy) above which to consider that a position of the sequence is enriched.
+#' @param group specific cluster to be analysed (e.g. in case of Cluster A, provide: "cluster_A").
+#' @param subtype mutation type: c("C_A", "C_G", "C_T", "T_A", "T_C", "T_G") (e.g. for evaluating C>A mutations, provide: "C_A" ).
+#' @param selection_case "rec" if wanted to analyse recurrent mutations or "unique" if wanted to analyse non-recurrent mutations.
+#' @param prior_genome corresponding proportion of each nucleotide in the genome.
+#' @param Dir_toSave_logoplot directory to save the sequence logo plot.
+#'
+#' @return
+#'
+SequenceLogo21 <- function(file, num_bp_context, threshold, group, subtype, selection_case, prior_genome, Dir_toSave_logoplot){
+  
+  # 1. LOAD DATA and determine some variables
+  # data counts
   loaded <- load(file)
   data <- get(loaded)
   
   # total length of the sequence to plot
-  sequence_length <- (length_aroundMut*2)+1
+  sequence_length <- (num_bp_context*2)+1
   
-    # number of samples
+  # number of samples
   n <- data[which(data[,ceiling(sequence_length/2)] != 0), ceiling(sequence_length/2)]
   n <- max(n)  #n[[1]] #this is the number of sequences for this cluster and subtype (not anymore)
   n_commas <- formatC(n, big.mark=",")
   
-    # mutation
+  # mutation
   mutation <- gsub("_", ">", subtype)
-    
-    # some parameters for the actual plot
+  
+  # some parameters for the actual plot
   if(subtype %in% c("T_A", "T_C", "T_G")){
     ymaxrect <- 1.76
     yminrectsubtype <- 1.78
@@ -123,13 +158,13 @@ SequenceLogo21 <- function(file, length_aroundMut, threshold, group, subtype, se
     R_frequency_prior <- prob_bases_seq_rev * log2(prob_bases_seq_rev_prior)
     
     information_content <- colSums(R_frequency_prior, na.rm=TRUE)
-
+    
     
     # 3. SELECT ENRICHED POSITIONS OVER DECIDED THRESHOLD 
     
     selected_positions <- which(information_content > threshold)
     
-
+    
     # 4. PLOT LOGO
     
     if(length(selected_positions) > 1){
@@ -138,7 +173,7 @@ SequenceLogo21 <- function(file, length_aroundMut, threshold, group, subtype, se
       continuous_pos <- seq(min(selected_positions), max(selected_positions),by=1)
       
       if(length(continuous_pos) == length(selected_positions)){    # (Case 1) Selected positions continuous -> UNGAPPED MOTIF
-
+        
         # LOGO 21 positions. Function [5]
         basic_LogoPlot <- plotLogo21(R_frequency_prior, sequence_length, yminrectsubtype, ymaxrectsubtype, ytext, group, subtype, n_commas) #input data
         
@@ -150,7 +185,7 @@ SequenceLogo21 <- function(file, length_aroundMut, threshold, group, subtype, se
         
         # Plot SequenceLogo of 21 positions
         basic_LogoPlot <- plotLogo21(R_frequency_prior, sequence_length, yminrectsubtype, ymaxrectsubtype, ytext, group, subtype, n_commas)
-  
+        
         # Selected positions
         Breaks <- c(0, which(diff(selected_positions) != 1), length(selected_positions)) 
         ind_selections <- sapply(seq(length(Breaks) - 1), function(i) selected_positions[(Breaks[i] + 1):Breaks[i+1]]) 
@@ -166,35 +201,28 @@ SequenceLogo21 <- function(file, length_aroundMut, threshold, group, subtype, se
       
       
     }else{                                                         # (Case 3) Normal plot with nothing selected
-           
+      
       # Plot SequenceLogo of 21 positions
       basic_LogoPlot <- plotLogo21(R_frequency_prior, sequence_length, yminrectsubtype, ymaxrectsubtype, ytext, group, subtype, n_commas)
-        # save plot
+      # save plot
       ggsave(paste0(Dir_toSave_logoplot,"LogoPlot_",group,"_",selection_case,"_",subtype,"_seq21.pdf"),  width = 30, height = 20, units = "cm", limitsize = FALSE)
       
     }
     
     
   }
-
+  
   
 }
 
 
-
-
-## CALL THE FUNCTION: Example for recurrent C>T mutations in Cluster G.
-
+## CALLING THE FUNCTION: Example for recurrent C>T mutations in Cluster G.
 length_aroundMut <- 10
 threshold <- 0.25
 group <- "cluster_G"
 subtype <- "C_T"
 selection_case <- "rec"
-prior_genome <- c(0.2952696, 0.2044550, 0.2045758, 0.2956996)
-names(prior_genome) <- c("A","C","G","T")
 file <- paste0(path_to_file_directory,"/SeqContext_counts_",group,"_",selection_case,"_",subtype,"_21positions.RData")
 Dir_toSave_logoplot <- getwd()
 
-  # calling the function:
 SequenceLogo21(file, length_aroundMut, threshold, group, subtype, selection_case, prior_genome, Dir_toSave_logoplot)
-
