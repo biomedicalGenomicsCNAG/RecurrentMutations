34a2e23bfad241e509d2f133fbd43b5f85378097
Miranda <miranda.stobbe@cnag.crg.eu>
2019-09-16 17:09:08 +0200

    Update Fig4_feature_association_2_clusters.R

--- Clustering/Code/Fig4_feature_association_2_clusters.R
+++ Clustering/Code/Fig4_feature_association_2_clusters.R
@@ -4,28 +4,30 @@ library(reshape2)
 
 #' Correct the p-values for the results of the v tests (features versus clusters) for multiple testing.
 #' @param res_pca: PCA object
-#' @return descr_var_hcpc: all statisticially significant association (p < 0.05) after multiple testing correction.
-correctPvalAssociations4MultTest <- function(res_pca){
+#' @param num_clusters: number of clusters
+#' @return data.frame with all statisticially significant associations (p < 0.05) after multiple testing correction.
+correctPvalAssociations4MultTest <- function(res_pca, num_clusters){
 
+    num_features <- nrow(res_pca$var$contrib)
     set.seed(10)
-    res_hcpc_th_prob1 <- HCPC(res_pca, nb.clust=16,max=20,graph = FALSE,consol=TRUE, proba=1)
+    res_hcpc_th_prob1 <- HCPC(res_pca, nb.clust=num_clusters,max=20,graph = FALSE,consol=TRUE, proba=1)
     
-    all_pval <- as.data.frame(matrix(nrow=672, ncol=2))
+    all_pval <- as.data.frame(matrix(nrow=(num_features * num_clusters), ncol=2))
     colnames(all_pval) <- c("pval", "clust_num")
     
     curRow <- 1
     
-    for(i in 1:16){
+    for(i in 1:num_clusters){
       curCluster <- res_hcpc_th_prob1$desc.var$quanti[[i]]
-      all_pval[curRow:(curRow+41),"pval"] <- curCluster[,"p.value"]
-      all_pval[curRow:(curRow+41),"clust_num"] <- i
-      curRow <- curRow + 42
+      all_pval[curRow:(curRow+num_features-1),"pval"] <- curCluster[,"p.value"]
+      all_pval[curRow:(curRow+num_features-1),"clust_num"] <- i
+      curRow <- curRow + num_features
     }
     all_pval$pval_adj <- p.adjust(all_pval$pval, method="BY")
     
     descr_var_hcpc <- res_hcpc_th_prob1$desc.var$quanti
     
-    for(i in 1:16){
+    for(i in 1:num_clusters){
       
       descr_var_hcpc[[i]][,"p.value"] <- all_pval[which(all_pval$clust_num == i), "pval_adj"]
       descr_var_hcpc[[i]] <- descr_var_hcpc[[i]][which(descr_var_hcpc[[i]][,"p.value"] < 0.05),]
@@ -35,24 +37,23 @@ correctPvalAssociations4MultTest <- function(res_pca){
 }
 
 #' Plot the association between the features and the clusters
-#' @param descr_var_hcpc: all statisticially significant association (p < 0.05) after multiple testing correction.
+#' @param descr_var_hcpc: all statisticially significant associations (p < 0.05) after multiple testing correction.
 #' @param feature_names: names of the features
+#' @param cluster_labels: labels of the clusters
 #' @param resultsDir: directory to store the resulting plot  
-plotAssociationOfFeatures2Clusters <- function(descr_var_hcpc, feature_names, resultsDir)
+plotAssociationOfFeatures2Clusters <- function(descr_var_hcpc, feature_names, cluster_labels, resultsDir, plotname)
 {
-  clusters <-  c("C","A","B","D","G","E","F","I","K","O","L","M","H","N","P","J")
-
   num_clusters <- length(descr_var_hcpc)
   
   features2vtestVal <- as.data.frame(matrix(data=0,nrow=length(feature_names), ncol=num_clusters))
   rownames(features2vtestVal) <- feature_names
   
-  colnames(features2vtestVal) <- clusters
+  colnames(features2vtestVal) <- cluster_labels
   
   for(i in 1:num_clusters)
   {
     cur_vtestValues <- descr_var_hcpc[[i]][,"v.test"]
-    features2vtestVal[names(cur_vtestValues),clusters[i]] <- cur_vtestValues
+    features2vtestVal[names(cur_vtestValues),cluster_labels[i]] <- cur_vtestValues
   }
   
   features2vtestVal$feature_names <- rownames(features2vtestVal)
@@ -71,5 +72,5 @@ plotAssociationOfFeatures2Clusters <- function(descr_var_hcpc, feature_names, re
     scale_fill_gradient2(low="black", high="grey90", na.value = "white", mid="grey45") +
     scale_x_discrete(expand=c(0,0)) +
     theme(axis.title=element_blank())
-  ggsave(file=paste(resultsDir, "/fig2_tileplot_featuresVtestValues.pdf", sep=""))
+  ggsave(file=paste(resultsDir, "/", plotname, ".pdf", sep=""))
 }
